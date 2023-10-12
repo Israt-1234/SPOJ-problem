318d106d82cda0e83c861a7e4e01702607a3373c
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-10-12 08:47:10 +0600

    Icpc Hello-mock

---
+++ icpc.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n; cin>>n;
+  while(n--){
+    cout<<"Hello World!"<<endl;
+  }
+
+  return 0;
+
+}
+
