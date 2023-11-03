65a6a906fbcd63698b96ffc3666f25b6144952cf
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-11-03 13:08:35 +0600

    Codechef

---
+++ Chef Fantasy 11.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+        int n;
+        cin>>n;
+       cout<<n*(n-1)<<endl;
+       // int k = pow(2, n);
+      //  cout<<k-2<<endl;
+	}
+	return 0;
+}
