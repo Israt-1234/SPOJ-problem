a81fa24cad0df2e218ff59a6389c3847231fcbe4
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-11-16 01:45:19 +0600

    Add files via upload

---
+++ Apples and Bananas.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout<<"Yes"<<endl
+#define No  cout<<"No"<<endl
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+   int a,b;cin>>a>>b;
+   cout<<a+b<<endl;
+   return 0;
+}
