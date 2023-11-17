79658aaa8490221594a4000f1b16d05de14e5afb
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-11-17 08:51:35 +0600

    vjudge

---
+++ A - Av.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout<<"Yes"<<endl
+#define No  cout<<"No"<<endl
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    int t; cin>>t;
+    cout<<(double)t/4<<endl;
+}
