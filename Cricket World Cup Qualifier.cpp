f09ce0f1ab2d85f16ffbd3a88192531403af5645
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-11-23 01:45:44 +0600

    Codechef

---
+++ Cricket World Cup Qualifier.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+const int N=1005;
+int d[N][N],pref[N][N];
+int main()
+{
+   // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin>>n;
+    if(n>=12)
+    cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+        return 0;
+}
