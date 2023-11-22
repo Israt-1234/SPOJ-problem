d15eeca75c902b2197233f3b82392a6fb88bb7e3
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-11-23 01:46:22 +0600

    Codechef

---
+++ Worth of a Video.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+const int N=1005;
+int d[N][N],pref[N][N];
+int main()
+{
+   // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n;
+    cin>>n;
+    while(n--){
+     ll t; cin>>t;
+     cout<<(t*1000)*24<<endl;
+
+    }
+    return 0;
+}
