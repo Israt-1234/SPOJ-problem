b0fbdc6830f30137685785f69c961ec68e9287cd
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2023-12-08 23:48:48 +0600

    vjduge

---
+++ A - A131.cpp
@@ -0,0 +1,20 @@
+/*........Hard work and consistency is the only way to success........ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1005;
+//int d[N][N],pref[N][N];
+int main()
+{
+    // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin>>n;
+    while(n--){
+    int x,y;
+    cin>>x>>y;
+    int sum=(x+y)/2;
+    cout<<max(abs(x-sum),abs(y-sum))<<endl;
+    }
+        return 0;
+}
