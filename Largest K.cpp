45ad9f2d8a80fab6c32b7abf8f989e2c376530c1
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-02-07 22:45:44 +0600

    codechef

---
+++ Largest K.cpp
@@ -0,0 +1,18 @@
+/*........Hard work and consistency is the only way to success........ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1005;
+//int d[N][N],pref[N][N];
+int main()
+{
+    // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;
+    while(t--){
+        ll x,y;cin>>x>>y;
+        cout<<y/(x-1)<<endl;
+    }
+    return 0;
+}
