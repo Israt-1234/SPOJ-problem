4c3c2470bd4f6b41c9cb213d64c5ceab48a1892c
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-02-26 22:01:46 +0600

    Add files via upload

---
+++ A-145.cpp
@@ -0,0 +1,23 @@
+/*........Hard work and consistency is the only way to success........ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1005;
+//int d[N][N],pref[N][N];
+int main()
+{
+    // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;cin>>n;
+    while(n--){
+        int x,y;cin>>x>>y;
+        int k=x/y;
+        if(x>10*y)
+        cout<<"YES"<<endl;
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
