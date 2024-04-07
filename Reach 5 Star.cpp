6ad16f4fe9406084eb83239b4098da9061318931
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-04-07 11:19:20 +0600

    codechef

---
+++ Reach 5 Star.cpp
@@ -0,0 +1,20 @@
+/*........Hard work and consistency is the only way to success........ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1005;
+//int d[N][N],pref[N][N];
+int main()
+{
+    // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,k;cin>>n>>k;
+    int sum=n+k;
+    if(sum>=2000)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+
+    return 0;
+}
